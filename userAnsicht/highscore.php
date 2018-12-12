a18cd2c016006496e58518217772d6ba37b80f6c
Sebastian Wahl <swahl@student.tgm.ac.at>
2018-12-12 10:49:53 +0100

    Änderungen an den UserSachen

--- userAnsicht/highscore.php
+++ userAnsicht/highscore.php
@@ -6,3 +6,16 @@
  * Time: 19:30
  */
 $db = mysqli_connect('localhost', 'tokenverwaltung', '1234', 'tvs_datenbank');
+$userName = $_SESSION['userName'];
+
+
+if (isset($_GET['eventAnzeigen']))
+{
+    $_SESSION['eventEintragung'] = NULL;
+    header("Refresh:0; url=events.html");
+}
+if (isset($_GET['highscoreAnzeigen']))
+{
+    $_SESSION['highscore'] = NULL;
+    header("Refresh:0; url=highscore.html");
+}
